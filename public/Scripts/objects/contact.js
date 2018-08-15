7068ade75da2a86f9f3d4e26140cbd41ca8b9346
nusratarasg <nriaz2@my.centennialcollege.ca>
2018-08-15 11:26:22 -0400

    Class definition added

--- public/Scripts/objects/contact.js
+++ public/Scripts/objects/contact.js
@@ -2,13 +2,14 @@ let objects;
 (function () {
     class Contact {
         // your class definition goes here
-        constructor(name = "", number = "", email = "", address = "") {
-            this.name = name;
-            this.number = number;
-            this.email = email;
-            this.address = address;
+        constructor(firstName = "", lastName = "", contactNumber = "", emailAddress = "") {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.contactNumber = contactNumber;
+            this.emailAddress = emailAddress;
         }
     }
+
 
     objects.Contact = Contact;
 
